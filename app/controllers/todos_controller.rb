620c1fa241e0e28f4443c1400fb015acfd7531c5
WRHR <wrhreeves@gmail.com>
2020-07-17 16:40:35 -0600

    Add create action on todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,25 @@
 class TodosController < ApplicationController
     def index
-        @todos = Todos.all 
+        @todos = Todo.all 
         render json: @todos
+    end
+
+    def create
+        @todo = Todo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+        render json: @todo, status: :created
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+
+        @todo.destroy
+
+        render status: :no_content
     end
 
 end
